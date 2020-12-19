f6292c9663ea7ad694b04979a75ef0c185c564b4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-19 11:29:30 +0700

    Add the logic

--- app/src/main/java/io/erikrios/github/barvolume/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/barvolume/MainActivity.kt
@@ -13,5 +13,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnCalculate.setOnClickListener {
+            val length = binding.etLength.text.toString().trim()
+            val width = binding.etWidth.text.toString().trim()
+            val height = binding.etHeight.text.toString().trim()
+
+            val isValidInput = isValid(length, width, height)
+
+            if (isValidInput) {
+                val volume = length.toDouble() * width.toDouble() * height.toDouble()
+                binding.tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    private fun isValid(length: String, width: String, height: String): Boolean {
+        var isValid = true
+
+        when {
+            length.isEmpty() -> {
+                isValid = false
+                binding.etLength.error = getString(R.string.not_empty_error)
+            }
+            width.isEmpty() -> {
+                isValid = false
+                binding.etWidth.error = getString(R.string.not_empty_error)
+            }
+            height.isEmpty() -> {
+                isValid = false
+                binding.etHeight.error = getString(R.string.not_empty_error)
+            }
+        }
+
+        return isValid
     }
 }
